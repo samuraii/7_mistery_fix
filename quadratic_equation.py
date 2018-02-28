356f06daa7f1ec78959f765725a636e1fac31a93
Mikhail C <7thsamurai@mail.ru>
2018-02-28 10:47:44 +0300

    Правки по ревью для скрипта

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,9 +3,18 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
+
+    # Если дискриминант меньше нуля, то корней нет
+    if discriminant < 0:
+        return None, None
+
+    # Вычисляем корни по формуле
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
+
+    # Если дискриминант равен нулю, то корень один
+    # Иначе уравнение имеет два корня
     if discriminant == 0:
         return root1, None
     else:
-        return int(root1), int(root2)
+        return root1, root2
